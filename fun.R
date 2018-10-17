aaf706a5ba96db57b54d9d00f47ef2e977004b97
Jorge López Pérez <jorge.loper@gmail.com>
2018-10-17 13:34:42 +0200

    Update fun.R

--- fun.R
+++ fun.R
@@ -9,3 +9,9 @@ set_hyphen <- function(string) {
 
 # Remove accents (convert to ascii)
 iconv(keyword, to='ASCII//TRANSLIT')
+
+# Check date type
+isdate <- function(mydate, date_format = "%y-%m-%d") {
+  tryCatch(is.na(as.Date(mydate, date_format)),  
+           error = function(err) {FALSE})  
+}
